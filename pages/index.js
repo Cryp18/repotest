4abfb0b177d00f294b9cb1a033b196a53e84ca21
Cryp18 <ajreyes@intellisys.com.do>
2021-06-25 14:19:34 -0400

    not mg banana

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-       hola mundo me gusta el guineo maduro
+       hola mundo ya no me gusta el guineo
     </div>
   )
 }
