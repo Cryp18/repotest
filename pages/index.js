956d47b06fa10267d6726a27281a47d701931c22
cryp01 <ichigooncross@gmail.com>
2021-06-28 09:42:54 -0400

    feat: changes title

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-       hola mundo ejecutate a ve 
+      <h2>hellow word</h2>
     </div>
   )
 }
